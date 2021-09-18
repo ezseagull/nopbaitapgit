1197696babd4e346c9bb0ae76075854bc3dc21f4
ezseagull <80797568+ezseagull@users.noreply.github.com>
2021-09-18 15:59:30 +0700

    oop

---
+++ week1/src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static  String printHello() {
+        return "HelloWorld";
+    }
+    public static void main(String[] args) {
+        System.out.print(printHello());
+    }
+}
